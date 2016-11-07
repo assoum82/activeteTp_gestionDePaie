8549255fe881e3053f954e4739db91ca63330d1f
Fatima1930 <labonne1930@hotmail.fr>
2016-11-07 17:15:43 +0100

    j ai joute une commit

--- src/Teste.java
+++ src/Teste.java
@@ -7,23 +7,23 @@ public class Teste  extends JPanel {
         Employe exemple[] = new Employe[3];
 
         exemple[0] = new Employe("Salim", "PDG");
-        exemple[0].setSalaire(100000);
+        exemple[0].setSalaire(100000 DA);
         exemple[0].chequePaie();
         exemple[1] = new EmployeTempPartiel("Halima", "Secrétaire");
-        ((EmployeTempPartiel) exemple[1]).setMontant_heur(20000);
-        exemple[1].FeullePresence();
+        ((EmployeTempPartiel) exemple[1]).setMontant_heur(20000 DA);
+        exemple[1].FeuillePresence();
         exemple[1].chequePaie();
         exemple[2] = new EmployeTempPlein("AbdeHalim", "Agent de sécurité");
-        ((EmployeTempPlein)exemple[2]) .setMontant_semaine(15000);
-        exemple[2].FeullePresence();
-        ((EmployeTempPlein)exemple[2]).setPrime(500);
+        ((EmployeTempPlein)exemple[2]) .setMontant_semaine(15000 DA);
+        exemple[2].FeuillePresence();
+        ((EmployeTempPlein)exemple[2]).setPrime(500 DA);
         exemple[2].chequePaie();
 
 
         String[] ligne = {"Nom", "fonction", "Salaire"};
         String[][] data = {{exemple[0].getNom(), exemple[0].getJob(), exemple[0].getSalaire() + ""}
-                , {exemple[1].getNom(), exemple[1].getJob(), exemple[1].getSalaire() + ""}
-                , {exemple[2].getNom(), exemple[2].getJob(), exemple[2].getSalaire() + ""}};
+                          ,{exemple[1].getNom(), exemple[1].getJob(), exemple[1].getSalaire() + ""}
+                          ,{exemple[2].getNom(), exemple[2].getJob(), exemple[2].getSalaire() + ""}};
 
 
         JTable jt = new JTable(data, ligne);
@@ -33,8 +33,9 @@ public class Teste  extends JPanel {
         jt.setFillsViewportHeight(true);
         JScrollPane jps = new JScrollPane(jt);
 
-        String[][] data1 = {{exemple[1].getNom(), exemple[1].getJob(), ((EmployeTempPartiel)exemple[1]).calculPaie() + ""}
-                , {exemple[2].getNom(), exemple[2].getJob(), ((EmployeTempPlein)exemple[2]).calculPaie() + ""}};
+        String[][] data1 = {{exemple[0].getNom(), exemple[0].getJob(), ((EmployeTempPartiel)exemple[0]).calculPaie() + ""}
+                , {exemple[1].getNom(), exemple[1].getJob(), ((EmployeTempPlein)exemple[1]).calculPaie() + ""
+                  , exemple [2].getNom(),exemple[2].getjob(),((EmpyeTempsPlein)exexmple[2].calculPaie()+""}};
 
 
         JTable jt1 = new JTable(data1, ligne);
